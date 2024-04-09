adf02745c26bbb1650045850b86d2d335c1d9466
Diego Llanos <llanos1205@gmail.com>
2024-04-09 18:03:23 +1200

    review User.cshtml.cs
    reviewed this file

--- Pages/User.cshtml.cs
+++ Pages/User.cshtml.cs
@@ -9,16 +9,20 @@ namespace Travel_Manager.Pages
     [Authorize]
     public class UserModel : PageModel
     {
+        //this is a private read only variable should follow a _
         private readonly UserManager<ApplicationUser> userManager;
-
+        //this should be  a private read only variable and should follow a _
         public ApplicationUser? appUser;
 
         public UserModel(UserManager<ApplicationUser> userManager)
         {
             this.userManager = userManager;
         }
+        //this function should be async
+        //this function is missing documentation for description of feature
         public void OnGet()
         {
+            //here the "user" should be null validated
             var task = userManager.GetUserAsync(User);
             task.Wait();
             appUser = task.Result;
